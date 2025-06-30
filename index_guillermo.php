b74acf30ba801f60a13a3776317e1919f3582053
Guillermo Granadino <gr222755@alumno.udb.edu.sv>
2025-06-30 17:29:52 -0600

    Pagina Completa

---
+++ index_guillermo.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Un Poco Sobre Mí</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Un Poco Sobre Mí</h1>
+            <p>Soy José Guillermo Granadino Rivas y algunos de mis pasatiempos o actividades que disfruto hacer, son las siguientes:</p>
+        </header>
+
+        <main>
+            <h2 id="subtitle">Mis Pasatiempos</h2>
+
+            <div class="cont-pasatiempos">
+                <ul>
+                    <li class="pasatiempo">
+                        <img src="https://catnessgames.com/wp-content/uploads/2024/12/tipos-videojuegos-consolas-y-plataformas.jpg" alt="Videojuegos">
+                        <h3 class="texto-titulo">Jugar Videojuegos</h3>
+                        <p class="texto-descripcion">Me entretiene mucho jugar videojuegos, especialmente de estrategia, puzzles y aventuras.</p>
+                    </li>
+
+                    <li class="pasatiempo">
+                        <img src="https://sps-sport.com/img/cms/Blog/diferencias%20entre%20los%20balones%20de%20futbol%20sala%20y%20los%20de%20futbol%2011.jpg" alt="Fútbol">
+                        <h3 class="texto-titulo">Jugar Fútbol</h3>
+                        <p class="texto-descripcion">Jugar fútbol es una de mis actividades favoritas. Disfruto mucho estar en la cancha jugando en cualquier posición.</p>
+                    </li>
+
+                    <li class="pasatiempo">
+                        <img src="https://www.infobae.com/resizer/v2/FUG54XKBEFDGFEC3IJR6KYSQIM.jpg?auth=63a912e15fc001538ae5a1e5a39929d4ef32a21b8a9641d86e37cbb2d0dba3a7&smart=true&width=350&height=197&quality=85" alt="Escuchar Música">
+                        <h3 class="texto-titulo">Escuchar Música</h3>
+                        <p class="texto-descripcion">Me encanta escuchar música cristiana, me mantiene alegre y muy feliz.</p>
+                    </li>
+
+                    <li class="pasatiempo">
+                        <img src="https://blog.dema-argentina.com.ar/hubfs/dolor%20de%20cintura.png" alt="Tocar el Piano">
+                        <h3 class="texto-titulo">Tocar el Piano</h3>
+                        <p class="texto-descripcion">Tocar el piano me permite adorar a Dios y desarrollar habilidades en mis manos, manteniendolas ágiles.</p>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    </div>
+
+    <footer>
+        <p>&copy; 2025 Guillermo Granadino. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
